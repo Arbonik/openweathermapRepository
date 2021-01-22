f546f195aa1d44dd644669b34855a67502d5b09a
polina1441 <14042003dp@mail.ru>
2021-01-22 10:57:13 +0700

    test

--- app/src/androidTest/java/com/castprogramms/openweathermap/ui/now/NowViewModelTest.kt
+++ app/src/androidTest/java/com/castprogramms/openweathermap/ui/now/NowViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.castprogramms.openweathermap.ui.now
 
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.*
+import com.castprogramms.openweathermap.database.data.weather.WeatherDao
 import junit.framework.Assert.assertEquals
+import net.bytebuddy.implementation.InvokeDynamic.lambda
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -9,15 +11,17 @@ import org.junit.Test
 import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
 
 @RunWith(JUnit4::class)
 class NowViewModelTest{
 
     @get:Rule
-    lateinit var nowViewModel: NowViewModel
-
-    //@get:Rule
     //var rule: TestRule = InstantTaskExecutorRule()
+    private val weatherDao = mock(WeatherDao::class.java)
+
+    var nowViewModel = NowViewModel()
 
     @Before
     fun set_(){
@@ -28,7 +32,11 @@ class NowViewModelTest{
     @Test
     @Throws(Exception::class)
     fun current_user_observer_liveData(){
+        val present = Present()
         //val observer = lambdaMock<(String) -> Unit>()
+        val lifecycle = LifecycleRegistry(mock(LifecycleOwner::class.java))
+        lifecycle.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        //present.observeTitleChange(lifecycle, observer)
         nowViewModel.currentUnit.observeForever{
 
         }
@@ -37,5 +45,13 @@ class NowViewModelTest{
     @After
     fun after_test(){
 
+    }
+}
+class Present{
+    val titleLiveData = MutableLiveData<String>()
+    fun observeTitleChange(lifecycle: Lifecycle, observer: (String) -> Unit){
+        titleLiveData.observe({lifecycle}){ title ->
+            title?.let(observer)
+        }
     }
 }
